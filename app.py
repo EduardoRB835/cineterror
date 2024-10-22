ebb5e4bcf3524973d3c1770c4f95d13e9debd987
EduardoRB835 <141856911+EduardoRB835@users.noreply.github.com>
2024-10-22 14:25:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import base64
 app = Flask(__name__)
 
 peliculas = {
-    'La Sutancia': {'hora': '10:00 -12:20', 'disponible': 30, 'imagen': 'images/sustancia.webp'},
+    'La Sutancia': {'hora': '10:00 -12:20', 'disponible': 0, 'imagen': 'images/sustancia.webp'},
     'Hereditary': {'hora': '12:30-14:30', 'disponible': 82, 'imagen': 'images/Hereditary.webp'},
     'La primera profecía': {'hora': '14:45-17:05', 'disponible': 82, 'imagen': 'images/la_primera_profecia.jpg'}
 }
