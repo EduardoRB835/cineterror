8ec7c63c98651c2cf97ca6c4ad7dab057ec6f31d
EduardoRB835 <141856911+EduardoRB835@users.noreply.github.com>
2024-10-24 20:47:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 peliculas = {
     'La Sutancia': {'hora': '10:00 -12:20', 'disponible': 0, 'imagen': 'images/sustancia.webp'},
     'Hereditary': {'hora': '12:30-14:30', 'disponible': 76, 'imagen': 'images/Hereditary.webp'},
-    'La primera profecía': {'hora': '14:45-17:05', 'disponible': 81, 'imagen': 'images/la_primera_profecia.jpg'}
+    'La primera profecía': {'hora': '14:45-17:05', 'disponible': 76, 'imagen': 'images/la_primera_profecia.jpg'}
 }
 
 # Configuración de correo (modifica con tus datos)
